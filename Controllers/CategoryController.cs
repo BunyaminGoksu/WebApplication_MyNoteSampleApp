fc749f6986bdee4b6e33dbe994fbb37ab563e346
BunyaminGoksu <goksu-bunyamin@hotmail.com>
2022-06-27 00:07:13 +0300

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_MyNoteSampleApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        public IActionResult Edit()
+        {
+            return View();
+
+        }
+        public IActionResult Delete()
+        {
+            return View();
+
+        }
+        public IActionResult Details()
+        {
+            return View();
+
+        }
+    }
+}
